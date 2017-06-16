b7f50397ba46d8d51193701d3fad6a1c30fa1d4b
borjagarcia <borja.garcia@algios.com>
2017-06-16 10:57:41 +0200

    [FIX] Product configurator
    Que si solo hay custom values tambien siga para adelante el configurador

--- product_configurator_wizard/wizard/product_configurator.py
+++ product_configurator_wizard/wizard/product_configurator.py
@@ -763,7 +763,7 @@ class ProductConfigurator(models.TransientModel):
         cfg_step_lines = self.product_tmpl_id.config_step_line_ids
 
         if not cfg_step_lines:
-            if self.value_ids:
+            if self.value_ids or self.custom_value_ids:
                 return self.action_config_done()
             else:
                 self.state = 'configure'
